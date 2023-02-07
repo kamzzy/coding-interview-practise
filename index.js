4f2a73cb7973be0a66b9924178cae7159d0bb4c6
Precious Akams <preciousakams@yahoo.com>
2023-02-07 13:51:27 +0100

    check if object is an array

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ const reverseEntireSentence = reverseBySeparator(string, "");
 const reverseEachWord = reverseBySeparator(reverseEntireSentence, " ");
 
 console.log(reverseEachWord)
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// Q3: How to check if an object is an array or not? Provide some code.
+const arrayList = [1,2,4];
+if(Object.prototype.toString.call(arrayList) === '[object Array]') {
+  console.log('Array')
+}
