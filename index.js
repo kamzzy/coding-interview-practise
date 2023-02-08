f2d2d685f65d07ba1ec1af802e62e319c61ccd94
Precious Akams <preciousakams@yahoo.com>
2023-02-08 17:48:32 +0100

    check if a number is an integer

--- index.js
+++ index.js
@@ -59,3 +59,17 @@ while(arrayList.length) {
   arrayList.pop();
 }
 
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// Q5: How would you check if a number is an integer?
+
+// A very simply way to check if a number is a decimal or integer is to 
+// see if there is a remainder left when you divide by 1.
+
+function isInt(num) {
+  return num % 1 === 0;
+}
+
+console.log(isInt(4)); // true
+console.log(isInt(12.2)); // false
+console.log(isInt(0.3)); // false
