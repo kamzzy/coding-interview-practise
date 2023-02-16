ae046d79513f96a454bf1b46c6888d77f2b87845
Precious Akams <preciousakams@yahoo.com>
2023-02-16 21:01:51 +0100

    anagram and closure function

---
+++ midLevel.js
@@ -0,0 +1,77 @@
+// Q10: FizzBuzz Challenge 
+// Problem
+// Create a for loop that iterates up to 10 while outputting 
+// "fizz" at multiples of 3, "buzz" at multiples of 5 and 
+// "fizzbuzz" at multiples of 3 and 5.
+
+for(let i=1; i <=10; i++){
+  if((i % 3) === 0){
+    console.log('fizz')
+  } else if((i % 5) === 0) {
+    console.log('buzz')
+  }else if( (i % 15) === 0) {
+    console.log('fizzbuzz')
+  } else {
+    console.log(i)
+  }
+}
+
+// Q11: Given two strings, return true if they are anagrams of one another
+// Problem
+// For example: Mary is an anagram of Army
+
+const anagram = (word1, word2) => {
+  // change words to upper or toLowerCase
+// takeout white spaces
+// sort the words, join and compare them
+   firstWord = word1.toLowerCase().split("").sort().join("")
+   secondWord = word2.toLowerCase().split("").sort().join("")
+  return firstWord === secondWord
+}
+console.log(anagram("john", "nojh"))
+//??????????????????????????????????????
+// A closure is a function that preserves the outer scope in its inner scope.
+// example 1
+// function greeting() {
+//   let message = 'Hi';
+//   function sayHi() {
+//       console.log(message);
+//   }
+//   return sayHi;
+// }
+// let hi = greeting();
+// hi()
+// sayHi() is a closure
+// ?????????????????????????
+// Example 2
+// function greeting(message) {
+//   return function(name){
+//        return message + ' ' + name;
+//   }
+// }
+// let sayHi = greeting('Hi');
+// let sayHello = greeting('Hello');
+
+// console.log(sayHi('John')); // Hi John
+// console.log(sayHello('John')); // Hello John
+// sayHi and sayHello are closures. They share the same function body but store different scopes.
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Q12: How would you use a closure to create a private counter?
+// You can create a function within an outer function (a closure)
+//  that allows you to update a private variable but the variable wouldn't be accessible 
+//  from outside the function without the use of a helper function.
+
+const counterFunction = () => {
+  let counter = 0;
+  const closureFunction = () => {
+    return console.log(counter +=1)
+  }
+  return closureFunction
+}
+const caller = counterFunction()
+caller()
+caller()
+caller()
+
+
+
