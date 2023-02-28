c70b9d492f96fa1fe8d5201f75d7c45b980ffb4f
Precious Akams <preciousakams@yahoo.com>
2023-02-28 13:09:26 +0100

    global scope

--- midLevel.js
+++ midLevel.js
@@ -125,3 +125,22 @@ if (1) {
   k += typeof foo;
 }
 console.log(k); // output 1function
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+// Q15: What will the following code output?
+
+(function() {
+  var a = b = 5;
+})();
+
+console.log(b);
+// // Answer
+// The code above will output 5 even though it seems as if the variable was declared 
+// within a function and can't be accessed outside of it. This is because
+
+var a = b = 5;
+// is interpreted the following way:
+
+var a = b;
+b = 5;
+// But b is not declared anywhere in the function with var so
+// it is set equal to 5 in the global scope.
