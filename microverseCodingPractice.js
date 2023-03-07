7ad900e1c4531b8b1dfe93b0bff89fe450e4e66e
Precious Akams <preciousakams@yahoo.com>
2023-03-07 16:38:42 +0100

    removeNth from end

--- microverseCodingPractice.js
+++ microverseCodingPractice.js
@@ -23,15 +23,17 @@ KLargest([10, 21, 3, 4, 15, 36, 47, 28, 9, 20], 3) // [47, 36, 28]
 // Answer:
 // The worst case occurs when the partition process always 
 // picks greatest or smallest element as pivot.
+// ##########################
+// Remove Nth Node From End of List
+// Given the head of a linked list, 
+// remove the nth node from the end of the list and return its head.
 
+const removeNthFromEnd = (head, n) => {
+  const listLength = head.length;
+  const position = listLength - n;
+  head.splice(position, 1);
+  return head;
+ }
 
-
-
-
-
-
-
-
-
-
+removeNthFromEnd([1,2,3,4,5], 2) // [1,2,3,5]
 
